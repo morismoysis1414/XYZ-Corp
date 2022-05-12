3431e8198d0e08400357abb382c0ef4ddd3c7e3d
Moris Moysis <moris.moysis@capco.com>
2022-05-12 20:06:31 +0100

    additions

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 
 
+
 logging.basicConfig(filename='dump.log', level=logging.INFO)
 
 app = Flask(__name__)
@@ -20,26 +21,54 @@ def predict_loan():
   
     #Creating dataframe
     df=pd.DataFrame.from_records([data])
+    df_nlp=df['desc']
+    df=df.drop('desc',axis=1)
+
+    model_nlp = pickle.load(open('model_nlp', 'rb'))
+    vect_nlp=pickle.load(open('vect_nlp', 'rb'))
+    df_nlp=vect_nlp.transform(df_nlp)
+    prediction_nlp=model_nlp.predict(df_nlp)[0]
+    #prediction_nlp=prediction_nlp[0]
+
+    ohe_cols=['purpose','verification_status','home_ownership',
+    'initial_list_status','term'] #address
+    df_scale=df.drop(ohe_cols,axis=1)
+    df_non_scale=df[ohe_cols]
+
+    clust={0:'A',1:'B',2:'C',3:'D',4:'E',5:'F',6:'G'}
+
 
     #Unpickling classification model predicting default index
     model_class_def = pickle.load(open('model_class_def', 'rb'))
-    ohe_cols=['purpose','verification_status','home_ownership','initial_list_status','term'] #address
     ohe_class_def=pickle.load(open('ohe_class_def', 'rb'))
-    df_enc = pd.DataFrame(ohe_class_def.transform(df[ohe_cols]).toarray(),index=df.index)
+    df_enc = pd.DataFrame(ohe_class_def.transform(df[ohe_cols]).toarray(),
+    index=df.index)
     df_class_def=df.join(df_enc).drop(ohe_cols,axis=1)
     df_class_def.columns = df_class_def.columns.map(str)
     prediction_class_def=model_class_def.predict(df_class_def)
 
     #Unpickling regression model predicting interest rate
-    #df=pd.DataFrame.from_records([data])
     model_reg_int=pickle.load(open('model_reg_int', 'rb'))
     ohe_reg_int=pickle.load(open('ohe_reg_int', 'rb'))
-    df_enc = pd.DataFrame(ohe_reg_int.transform(df[ohe_cols]).toarray(),index=df.index)
+    df_enc = pd.DataFrame(ohe_reg_int.transform(df[ohe_cols]).toarray(),
+    index=df.index)
     df_reg_int=df.join(df_enc).drop(ohe_cols,axis=1)
     df_reg_int.columns = df_reg_int.columns.map(str)
     prediction_reg_int=model_reg_int.predict(df_reg_int)
     intr_rate=prediction_reg_int[0]
 
+
+    model_unsup=pickle.load(open('model_unsup', 'rb'))
+    ohe_unsup=pickle.load(open('ohe_unsup', 'rb'))
+    scaler_unsup=pickle.load(open('scaler_unsup', 'rb'))
+    df_scaled = pd.DataFrame(scaler_unsup.transform(df_scale),index=df_scale.index,columns=df_scale.columns)
+    df_unsup=df_scaled.join(df_non_scale)
+    df_enc = pd.DataFrame(ohe_unsup.transform(df[ohe_cols]).toarray(),
+    index=df.index)
+    df_unsup=df_unsup.join(df_enc).drop(ohe_cols,axis=1)
+    df_unsup.columns = df_unsup.columns.map(str)
+    prediction_unsup=model_unsup.predict(df_unsup)
+    grade_unsup=prediction_unsup[0]
 
     #Getting default loan case
     if prediction_class_def[0]==1:
@@ -49,7 +78,8 @@ def predict_loan():
         #df=pd.DataFrame.from_records([data])
         model_class_rec = pickle.load(open('model_class_rec', 'rb'))
         ohe_class_rec=pickle.load(open('ohe_class_rec', 'rb'))
-        df_enc = pd.DataFrame(ohe_class_rec.transform(df[ohe_cols]).toarray(),index=df.index)
+        df_enc = pd.DataFrame(ohe_class_rec.transform(df[ohe_cols]).toarray(),
+        index=df.index)
         df_class_rec=df.join(df_enc).drop(ohe_cols,axis=1)
         df_class_rec.columns = df_class_rec.columns.map(str)
         prediction_class_rec=model_class_rec.predict(df_class_rec)
@@ -61,7 +91,8 @@ def predict_loan():
             #df=pd.DataFrame.from_records([data])
             model_reg_rec = pickle.load(open('model_reg_rec', 'rb'))
             ohe_reg_rec=pickle.load(open('ohe_reg_rec', 'rb'))
-            df_enc = pd.DataFrame(ohe_reg_rec.transform(df[ohe_cols]).toarray(),index=df.index)
+            df_enc = pd.DataFrame(ohe_reg_rec.transform(df[ohe_cols]).toarray(),
+            index=df.index)
             df_reg_rec=df.join(df_enc).drop(ohe_cols,axis=1)
             df_reg_rec.columns = df_reg_rec.columns.map(str)
             prediction_reg_rec=model_reg_rec.predict(df_reg_rec)
@@ -72,14 +103,18 @@ def predict_loan():
             recoveries='0'
         
         #Getting outcome
-        outcome={'Loan Prediction: ':loan,'Recoveries: ':str(recoveries),'Predicted Interest Rate: ':str(intr_rate)} 
+        outcome={'Loan Prediction: ':loan,'Recoveries: ':str(recoveries)
+        ,'Predicted Interest Rate: ':str(intr_rate), 'Grade Unsupervised':clust[grade_unsup],
+        'Predicted Purpose':str(prediction_nlp)
+        } 
 
     #Getting non-default loan case    
     else:
         loan='Loan will not go default'
 
         #Getting outcome
-        outcome={'Loan Prediction: ':loan,'Predicted Interest Rate: ':str(intr_rate)} 
+        outcome={'Loan Prediction: ':loan,'Predicted Interest Rate: ':str(intr_rate),
+        'Grade Unsupervised':clust[grade_unsup],'Predicted Purpose':str(prediction_nlp)} 
 
     #Dumping outcome to JSON 
     return json.dumps(outcome)
